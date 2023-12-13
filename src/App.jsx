845e159ead8e7918bf4b191c64db00985bd7ef00
Amanda <amanda.ausejo@gmail.com>
2023-12-13 18:56:36 +0100

    fix exercise 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import { useEffect } from "react";
+import JokeCard from "./JokeCard";
 
 function App() {
-    const [joke, setJoke] = useState();
-
-    const fetchJokes = async () => {
-        try {
-            const response = await fetch(
-                "https://v2.jokeapi.dev/joke/Programming?type=twopart"
-            );
-            const obj = await response.json();
-            if (obj.error) {
-                throw new Error("Error, there is an error in the fetch");
-            }
-            console.log(obj);
-            setJoke(obj);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    useEffect(() => {
-        fetchJokes();
-    }, []);
-    return <></>;
+    return (
+        <>
+            <JokeCard />
+        </>
+    );
 }
 
 export default App;
---
+++ src/JokeCard.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const JokeCard = () => {
+    const [joke, setJoke] = useState();
+
+    const fetchJokes = async () => {
+        try {
+            const response = await fetch(
+                "https://v2.jokeapi.dev/joke/Programming?type=twopart"
+            );
+            const obj = await response.json();
+            if (obj.error) {
+                throw new Error("Error, there is an error in the fetch");
+            }
+            console.log(obj);
+            setJoke(obj);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchJokes();
+    }, []);
+
+    return <></>;
+};
+
+export default JokeCard;
